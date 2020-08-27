92bf5dc141563568847f71e648814a305138c7e1
Rudramani Singha <monacotime@outlook.com>
2020-08-27 16:41:02 +0530

    okay

--- scene.py
+++ scene.py
@@ -1,4 +1,5 @@
 import bpy
 
 print("hi")
-bpy.ops.mesh.primitive_cube_add(location=(0,0,0))
+# bpy.ops.mesh.primitive_cube_add(location=(0,0,0))
+bpy.ops.mesh.primitive_cube_add( size=1, location=(0,0,0))
