db815dafcb8a1361549761ccf2284c48e946ab2d
Rudramani Singha <monacotime@outlook.com>
2020-08-27 11:36:50 +0530

    any

--- timelapse.py
+++ timelapse.py
@@ -1,12 +1,17 @@
 import numpy as np
 from PIL import ImageGrab
 import cv2
+import time
+import os
+
+count = 0
+main_dir = r"C:\Users\monac\Videos\speed\timelapse"
+cur_dir = f"{main_dir}\{int(time.time())}"
+os.mkdir(cur_dir)
 
 while(True):
-    printscreen_pil =  ImageGrab.grab()
-    printscreen_numpy =   np.array(printscreen_pil.getdata(),dtype='uint8')\
-    .reshape((printscreen_pil.size[1],printscreen_pil.size[0],3)) 
-    cv2.imshow('window',printscreen_numpy)
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
+    printscreen_pil =  ImageGrab.grab(bbox=None)
+    printscreen_pil.save(f"{cur_dir}\{count}.jpg","JPEG")
+    print("ss no: ", count)
+    count+= 1
+    time.sleep(10)
