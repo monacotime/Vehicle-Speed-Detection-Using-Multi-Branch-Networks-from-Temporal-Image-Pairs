822c575861f0ce8d82257d0b93d6a284a44fa3a7
Rudramani Singha <monacotime@outlook.com>
2020-08-28 20:56:28 +0530

    finals

--- scene.py
+++ scene.py
@@ -1,14 +1,7 @@
 import bpy
 import random
 import pickle
-#------------------------------------------------
-#print("hi")
-# bpy.ops.mesh.primitive_cube_add(location=(0,0,0))
-# bpy.ops.mesh.primitive_cube_add( size=1, location=(0,0,0))
-# print(dir(bpy.data.objects["Car"]))
-# print(dir(bpy.data.textures.get("body")))
-# bpy.data.materials["Body"].diffuse_color[0, 0.00289276, 1, 1]
-#------------------------------------------------
+random.seed(100)
 
 #Global vars
 active_object = bpy.context.view_layer.objects.active 
@@ -18,6 +11,7 @@ Car_body_mat = bpy.data.materials["Body"]
 train_before = r"C:\Users\monac\Documents\GitHub\DeepForSpeed\Dataset\Train\Before"
 train_after = r"C:\Users\monac\Documents\GitHub\DeepForSpeed\Dataset\Train\After"
 train_speed_txt = r".\Dataset\Train\speed.txt"
+train_licence_txt = r".\Dataset\Train\license.txt"
 license_plate_list = ["MH 1010", "BX 2050", "KP 3001", "PX 8029", "GM 8980"]
 car_colour_list = [
     [1, 0, 0, 1], 
@@ -29,38 +23,35 @@ count = 0
 speed_list = []
 plate_list = []
 
-#Fns
-# def select_car():
-#     bpy.ops.object.select_all(action='DESELECT')
-#     active_object = Car
-#     Car.select_set(True)
-
-# def move_car(dist = 1):
-#     select_car()
-#     # bpy.ops.transform.translate(value = (0, dist, 0))
-#     bpy.context.object.location[1] = 3
-
 def speed_to_dist(speed):
     # mov = "movement in meters in 0.25s:"
     return round((speed*1000)/(60*60*4),5)
 
-def init_pos():
-    return round(random.uniform(3,5),5)
-
 #Execs
 
-Plate_text.data.body = random.choice(license_plate_list)
-Car_body_mat.diffuse_color[random.choice(car_colour_list)]
+for i in range(1000):
+    Car_body_mat.diffuse_color = random.choice(car_colour_list)
+    
+    plate = random.choice(license_plate_list)
+    plate_list.append(plate)
+    Plate_text.data.body = plate
 
-# speed = random.randint(0,150)
-# speed_list.append(speed)
-# Car.location.y = init_pos()
-# bpy.context.scene.render.filepath = f"{train_before}\{count}.jpg"
-# bpy.ops.render.render(write_still = True)
+    speed = random.randint(0,150)
+    speed_list.append(speed)
 
-# Car.location.y += speed_to_dist(speed)
-# bpy.context.scene.render.filepath = f"{train_after}\{count}.jpg"
-# bpy.ops.render.render(write_still = True)
+    Car.location.y = round(random.uniform(3,5),5)
+    bpy.context.scene.render.filepath = f"{train_before}\{count}.jpg"
+    bpy.ops.render.render(write_still = True)
 
-# with open(train_speed_txt, "wb") as fp:
-#     pickle.dump(speed_list, fp)
+    Car.location.y += speed_to_dist(speed)
+    bpy.context.scene.render.filepath = f"{train_after}\{count}.jpg"
+    bpy.ops.render.render(write_still = True)
+
+    count += 1
+    print("no of pics:", count)
+
+with open(train_speed_txt, "wb") as fp:
+    pickle.dump(speed_list, fp)
+
+with open(train_licence_txt, "wb") as fp:
+    pickle.dump(plate_list, fp)
--- temp.py
+++ temp.py
@@ -16,9 +16,32 @@
 
 # print("init pos:", init_pos())
 
-import pickle
+#Fns
+# def select_car():
+#     bpy.ops.object.select_all(action='DESELECT')
+#     active_object = Car
+#     Car.select_set(True)
 
-with open(r".\Dataset\Train\speed.txt", "rb") as fp:   # Unpickling
-    b = pickle.load(fp)
+# def move_car(dist = 1):
+#     select_car()
+#     # bpy.ops.transform.translate(value = (0, dist, 0))
+#     bpy.context.object.location[1] = 3
 
-print(b)
+#------------------------------------------------
+#print("hi")
+# bpy.ops.mesh.primitive_cube_add(location=(0,0,0))
+# bpy.ops.mesh.primitive_cube_add( size=1, location=(0,0,0))
+# print(dir(bpy.data.objects["Car"]))
+# print(dir(bpy.data.textures.get("body")))
+# bpy.data.materials["Body"].diffuse_color = [0, 0.00289276, 1, 1]
+#------------------------------------------------
+
+# import pickle
+
+# with open(r".\Dataset\Train\speed.txt", "rb") as fp:   # Unpickling
+#     b = pickle.load(fp)
+
+# print(b)
+
+x = [1,3,4,5,6]
+print(x[1,2])
